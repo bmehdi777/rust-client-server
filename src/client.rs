b96df9e7c0b8e4a6c7af2e9854c3663eab2e53ca
Bentouati Mehdi <mehdi.bentouati76@gmail.com>
2023-11-16 00:59:31 +0100

    feat: fix client server message

--- src/client.rs
+++ src/client.rs
@@ -1,16 +1,33 @@
-use basic_serv_client::SERVER_PORT;
-use std::net::TcpStream;
+use basic_serv_client::{Message, MessageType, SERVER_PORT};
+use std::io::prelude::*;
+use std::net::{Ipv4Addr, TcpStream};
 
 fn main() -> std::io::Result<()> {
     let srv_addr: String = handle_server_addr()?;
+
     println!("INFO: connecting to server...");
-    TcpStream::connect(format!("192.168.1.22:{}", SERVER_PORT));
+    let mut stream = TcpStream::connect(format!("192.168.1.22:{}", SERVER_PORT))?;
+
+    let connect_msg: Message = Message::new(
+        String::new(),
+        String::from("username"),
+        MessageType::ConnectionInit,
+    );
+    let data: Vec<u8> = connect_msg.into();
+    println!("data: {:?}", data);
+    stream.write(&data.to_owned())?;
+    println!("INFO: successly connected to server. Waiting for a message...");
 
     Ok(())
 }
 
 fn handle_server_addr() -> std::io::Result<String> {
+    print!("Enter a server address: ");
+    let _ = std::io::stdout().flush();
+
     let mut buffer = String::new();
     std::io::stdin().read_line(&mut buffer)?;
-    Ok(buffer)
+
+    //Ok(buffer)
+    Ok(String::from("192.168.1.22"))
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-use std::net::Ipv4Addr;
-
 pub const MAX_PACKET_SIZE: usize = 65535;
 pub const SERVER_PORT: u32 = 6969;
 
@@ -23,34 +21,69 @@ impl From<u8> for MessageType {
 }
 
 pub struct Message {
-    pub length: usize,
+    pub length_content: u32,
+    pub length_username: u32,
     pub message_type: MessageType,
-    pub source: Ipv4Addr,
+    pub username: String,
     pub content: String,
+}
+
+impl Message {
+    pub fn new(content: String, username: String, message_type: MessageType) -> Self {
+        let length_content: u32 = content
+            .len()
+            .try_into()
+            .expect("Can't parse usize into u32");
+
+        let length_username: u32 = username
+            .len()
+            .try_into()
+            .expect("Can't parse usize into u32");
+
+        Message {
+            length_content,
+            length_username,
+            message_type,
+            username,
+            content,
+        }
+    }
 }
 
 impl std::fmt::Display for Message {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "Length: {}\nMessage Type: {:?}\nSource: {}\nContent: {}\n",
-            self.length, self.message_type, self.source, self.content
+            "\nMessage Type: {:?}\nLength username: {}\nUsername: {}\nLength content: {} \nContent: {}\n",
+             self.message_type, self.length_username, self.username,self.length_content, self.content
         )
     }
 }
 
 impl From<Vec<u8>> for Message {
     fn from(raw_bytes: Vec<u8>) -> Self {
-        let length: usize = unsafe { raw_bytes[0..4].align_to::<usize>() }.1[0];
-        let message_type: MessageType = raw_bytes[4].into();
-        let source: Ipv4Addr =
-            Ipv4Addr::new(raw_bytes[5], raw_bytes[6], raw_bytes[7], raw_bytes[8]);
-        let content: String = String::from_utf8(raw_bytes[9..9 + length + 1].to_vec())
+        let length_content: u32 = convert_u8s_to_u32(&raw_bytes[0..4]);
+        let length_username: u32 = convert_u8s_to_u32(&raw_bytes[4..8]);
+
+        let length_content_usize: usize = length_content as usize;
+        let length_username_usize: usize = length_username as usize;
+
+        let message_type: MessageType = raw_bytes[8].into();
+
+        let max_length_username: usize = 9 + length_username_usize;
+
+        let username: String = String::from_utf8(raw_bytes[9..max_length_username].to_vec())
             .expect("Invalid UTF-8 sequence.");
+        let content: String = String::from_utf8(
+            raw_bytes[max_length_username..max_length_username + length_content_usize].to_vec(),
+        )
+        .expect("Invalid UTF-8 sequence.");
+
         Message {
-            length,
+            length_content,
+            length_username,
             message_type,
-            source,
+            username,
             content,
         }
     }
@@ -58,17 +91,24 @@ impl From<Vec<u8>> for Message {
 
 impl From<Message> for Vec<u8> {
     fn from(message: Message) -> Self {
-        let src: [u8; 4] = message.source.octets();
+        let mut raw_bytes: Vec<u8> = Vec::new();
+
+        let username: Vec<u8> = message.username.into_bytes();
         let content: Vec<u8> = message.content.into_bytes();
-        let mut raw_bytes = vec![
-            message.length as u8,
-            message.message_type as u8,
-            src[0],
-            src[1],
-            src[2],
-            src[3],
-        ];
+
+        raw_bytes.extend(message.length_content.to_ne_bytes());
+        raw_bytes.extend(message.length_username.to_ne_bytes());
+        raw_bytes.push(message.message_type as u8);
+        raw_bytes.extend(username);
         raw_bytes.extend(content);
+
         raw_bytes
     }
+}
+
+fn convert_u8s_to_u32(array: &[u8]) -> u32 {
+    ((array[0] as u32) << 0)
+        + ((array[1] as u32) << 8)
+        + ((array[2] as u32) << 16)
+        + ((array[3] as u32) << 24)
 }
